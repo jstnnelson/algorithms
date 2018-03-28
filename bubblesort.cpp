8d090001277863f210abe72d9b4f1991a25a3c9a
Justin Nelson <jstnnelson@gmail.com>
2018-03-27 22:57:03 -0500

    Added Bubblesort

---
+++ bubblesort.cpp
@@ -0,0 +1,35 @@
+//
+//Bubble Sort Example
+//Worst case O(n^2)
+//Best Case O(n)
+//Average Case O(n^2)
+//
+//
+
+#include <iostream>
+
+
+
+int main(){
+  int array[] = {4,2,8,1,6,5,9,3,7};
+  bool swapped;
+
+  do{
+    swapped = false;
+    for(int i=1;i<sizeof(array)/sizeof(array[0]);i++){
+      if(array[i-1] > array[i]){
+        swapped = true;
+        int temp = array[i];
+        array[i] = array[i-1];
+        array[i-1] = temp;
+      }
+    }
+  }while(swapped);
+      
+
+  for(int i=0;i<sizeof(array)/sizeof(array[0]);i++){
+    std::cout << array[i];
+  }
+  std::cout << std::endl;
+  return 0;
+}
